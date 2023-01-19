ee5825ead893c1880e3630896119fc5b13e48f21
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-01-19 23:03:22 +0100

    Fix linters error

--- src/modules/work.js
+++ src/modules/work.js
@@ -1,7 +1,7 @@
 export default class Work {
-    constructor(description) {
-        this.description = description;
-        this.completed = false;
-        this.index = 0;
-    }
+  constructor(description) {
+    this.description = description;
+    this.completed = false;
+    this.index = 0;
+  }
 }
