6a07836f4e6ad72c37ee9401379e37c683a2adb3
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-26 13:46:57 +0300

    Add tests for addition and deletion of tasks

--- functionality.test.js
+++ functionality.test.js
@@ -5,24 +5,18 @@
 import functions from "./src/modules/functionality";
 import LocalStorage from "./src/modules/localstorage";
 
-let list;
-if (LocalStorage.getData() === null) {
-  list = [];
-} else {
-  list = LocalStorage.getData();
-}
-
-test('should add a new task', () => {
+describe('functionality methods', () => {
+  test('should add a new task', () => {
     functions.addtask('hello world')
-    expect(list[0].description).toStrictEqual('hello world');
+    expect(LocalStorage.getData()).toStrictEqual([{ description: 'hello world', completed: false, index: 0}]);
+  })
+  test('should add a new task', () => {
+    functions.addtask('second input')
+    expect(LocalStorage.getData()).toStrictEqual([{ description: 'hello world', completed: false, index: 0},{ description: 'second input', completed: false, index: 1}]);
+  })
+  test('should remove a task', () => {
+    functions.removeTask(0);
+    expect({ description: 'second input', completed: false, index: 0});
+  })
 })
 
-test('should add another new task', () => {
-    functions.addtask('hello world')
-    expect(localStorage.length).toBe(2);
-})
-
-test('should remove a task', () => {
-    functions.removeTask();
-    expect(localStorage.length).toBe(1);
-})
