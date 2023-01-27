cf9fe85e2bdfd390cd5deb7129e4e1409ed9d7f2
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-27 14:33:03 +0300

    Add DOM manipulation to test

--- tests/functionality.test.js
+++ tests/functionality.test.js
@@ -6,16 +6,35 @@ import functions from '../src/modules/functionality.js';
 import LocalStorage from '../src/modules/localstorage.js';
 
 describe('Add Task method', () => {
+  beforeEach(() => {
+  document.body.innerHTML =
+    '<div>' +
+    '  <ul id="allLists"></ul>' +
+    '</div>';
+  })
+
   test('should add a new task', () => {
     functions.addtask('hello world');
     let list = LocalStorage.getData();
-      expect(list[0].description).toStrictEqual('hello world')
+    expect(list[0].description).toStrictEqual('hello world')
+    const displayed =document.querySelector('#allLists')
+    list.forEach((item) => {
+      displayed.innerHTML = `<li>${item}</li>`
+    })
+    const listed = document.querySelectorAll('#allLists li')
+    expect(listed).toHaveLength(1);
   });
 
   test('should add another new task', () => {
     functions.addtask('second input');
     let list = LocalStorage.getData();
     expect(list[1].description).toStrictEqual('second input');
+  });
+
+  test('should add another new task', () => {
+    functions.addtask('third input');
+    let list = LocalStorage.getData();
+    expect(list[2].description).toStrictEqual('third input');
   });
 
   test('should add another new task', () => {
